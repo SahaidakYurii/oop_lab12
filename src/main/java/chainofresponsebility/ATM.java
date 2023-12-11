5d169f2821790909526580ea2df852ad1c33745c
SahaidakYurii <sahaidak.pn@ucu.edu.ua>
2023-12-11 18:57:57 +0200

    minor changes

--- src/main/java/chainofresponsebility/ATM.java
+++ src/main/java/chainofresponsebility/ATM.java
@@ -9,7 +9,6 @@ public class ATM {
 
     public static void main(String[] args){
         ATM terminal = new ATM(1025);
-        Handler handler1000 = new Handler1000(1000);
         Handler handler500 = new Handler500(500);
         Handler handler200 = new Handler200(200);
         Handler handler100 = new Handler100(100);
@@ -17,7 +16,6 @@ public class ATM {
         Handler handler20 = new Handler20(20);
         Handler handler10 = new Handler10(10);
         Handler handler5 = new Handler5(5);
-        handler1000.setNext(handler500);
         handler500.setNext(handler200);
         handler200.setNext(handler100);
         handler100.setNext(handler50);
@@ -26,7 +24,7 @@ public class ATM {
         handler10.setNext(handler5);
         try {
             if (terminal.amount >= 1)
-                handler1000.process(terminal.amount);
+                handler500.process(terminal.amount);
             else
                 System.out.println("Please change value (>= 1)");
         } catch (IllegalArgumentException e){
--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -6,7 +6,6 @@ public class DemoTest {
     @Test
     public void testMain(){
         ATM terminal = new ATM(1525);
-        Handler handler1000 = new Handler1000(1000);
         Handler handler500 = new Handler500(500);
         Handler handler200 = new Handler200(200);
         Handler handler100 = new Handler100(100);
@@ -14,20 +13,18 @@ public class DemoTest {
         Handler handler20 = new Handler20(20);
         Handler handler10 = new Handler10(10);
         Handler handler5 = new Handler5(5);
-        handler1000.setNext(handler500);
         handler500.setNext(handler200);
         handler200.setNext(handler100);
         handler100.setNext(handler50);
         handler50.setNext(handler20);
         handler20.setNext(handler10);
         handler10.setNext(handler5);
-        Assertions.assertEquals(null, handler1000.process(terminal.amount));
+        Assertions.assertEquals(null, handler500.process(terminal.amount));
     }
 
     @Test
     public void testATM(){
         ATM terminal = new ATM(0);
-        Handler handler1000 = new Handler1000(1000);
         Handler handler500 = new Handler500(500);
         Handler handler200 = new Handler200(200);
         Handler handler100 = new Handler100(100);
@@ -35,13 +32,12 @@ public class DemoTest {
         Handler handler20 = new Handler20(20);
         Handler handler10 = new Handler10(10);
         Handler handler5 = new Handler5(5);
-        handler1000.setNext(handler500);
         handler500.setNext(handler200);
         handler200.setNext(handler100);
         handler100.setNext(handler50);
         handler50.setNext(handler20);
         handler20.setNext(handler10);
         handler10.setNext(handler5);
-        Assertions.assertEquals(null, handler1000.process(terminal.amount));
+        Assertions.assertEquals(null, handler500.process(terminal.amount));
     }
 }
